3d95e078caf4b045907317bf684d7e1caeb8ac98
Sihayah <sihaya_a@hotmail.com>
2022-02-28 04:22:55 -0500

    moved portfolio content into array

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,22 +1,40 @@
 import React from 'react';
 
-import petProject from '../assets/landings/pet-project.PNG';
-import fff from '../assets/landings/forum-follows-func.PNG';
-import weatherDash from '../assets/landings/weather-dashboard.JPEG';
-
 const Portfolio = () => {
+    const projects = [
+        {
+            title: "Pet Project",
+            file: "pet-project.PNG",
+            repo: "https://github.com/mkohn4/pet-project",
+            deployment: "https://warm-springs-61498.herokuapp.com/post/1"
+        },
+        {
+            title: "Forum Follows Functions",
+            file: "forum-follows-func.PNG",
+            repo: "https://github.com/sihayah/forum_follows_functions",
+            deployment: "https://forum-follows-functions.herokuapp.com/"
+        },
+        {
+            title: "Weather Dashboard",
+            file: "weather-dashboard.JPEG",
+            repo: "https://github.com/sihayah/weather-dashboard",
+            deployment: "https://sihayah.github.io/weather-dashboard/"
+        }
+    ];
     return (
-        <>
-            <p>
-                this will be a portfolio
-            </p>
-            <div className="img-container">
-                <img src={petProject} />
-                <img src={fff} />
-                <img src={weatherDash} />                
-            </div>
-
-        </>
+        <div className="img-container d-flex flex-row">
+            {projects.map ((project) => (
+                <div className="project-container d-flex flex-column">
+                    <img src={require(`../assets/landings/${project.file}`)} alt={project.title} className="align-self-center"></img>
+                    <h4>{project.title}</h4>
+                    <span>
+                        <a href={project.repo}>REPO</a>
+                        <span> / </span>
+                        <a href={project.deployment}>DEPLOYMENT</a>     
+                    </span>                   
+                </div>
+            ))}
+        </div>
        
     )
 };
