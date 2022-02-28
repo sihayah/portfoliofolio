645203f4f8d1e19a2829e2249c0274728858f837
Sihayah <sihaya_a@hotmail.com>
2022-02-28 01:39:21 -0500

    added test images to portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import petProject from '../assets/landings/pet-project.PNG';
+import fff from '../assets/landings/forum-follows-func.PNG';
+import weatherDash from '../assets/landings/weather-dashboard.JPEG';
+
 const Portfolio = () => {
     return (
-        <p>
-            this will be a portfolio
-        </p>
+        <>
+            <p>
+                this will be a portfolio
+            </p>
+            <div className="img-container">
+                <img src={petProject} />
+                <img src={fff} />
+                <img src={weatherDash} />                
+            </div>
+
+        </>
+       
     )
 };
+
 
 export default Portfolio;
