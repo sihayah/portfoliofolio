c3b2afaba519109358f346bfc32eca11c8168ef9
Sihayah <sihaya_a@hotmail.com>
2022-02-28 05:43:46 -0500

    adjusted media queries for resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,35 +3,38 @@ import pdfIcon from '../assets/pdf-icon.png'
 
 const Resume = () => {
     return (
-        <section>
-            <div id="download-here">
-                <img src={pdfIcon} alt="pdf icon" id="pdf-icon" />                
-                <a href="../../Resume.pdf">
-                    download my resume here
-                </a>
-            </div>
-            <div className="skills">
+        <section id="resume-wrapper">
+            <div id="resume">
+                <div id="download-here">
+                    <img src={pdfIcon} alt="pdf icon" id="pdf-icon" />                
+                    <a href="../../Resume.pdf">
+                        download my resume here
+                    </a>
+                </div>
+                <div className="skills">
 
-                <ul>                
-                    <h4>
-                        [my skills]     
-                    </h4>   
-                    <li>HTML5</li>
-                    <li>CSS</li>
-                    <li>Javascript</li>
-                    <li>Rest APIs</li>
-                    <li>Git</li>
-                    <li>Node.js</li>
-                    <li>MySQL</li>
-                    <li>NoSQL</li>
-                    <li>Express</li>
-                    <li>MERN</li>
-                    <li>React</li>
-                    <li>GraphQL</li>
-                    <li>Agile Methodologies</li>
-                </ul>    
-            </div>     
+                    <ul>                
+                        <h4>
+                            [my skills]     
+                        </h4>   
+                        <li>HTML5</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                        <li>Rest APIs</li>
+                        <li>Git</li>
+                        <li>Node.js</li>
+                        <li>MySQL</li>
+                        <li>NoSQL</li>
+                        <li>Express</li>
+                        <li>MERN</li>
+                        <li>React</li>
+                        <li>GraphQL</li>
+                        <li>Agile Methodologies</li>
+                    </ul>    
+                </div>     
+            </div>
         </section>
+
 
     )
 };
