95151b5960dc2afdcd93ea3ced115becce71df1b
Sihayah <sihaya_a@hotmail.com>
2022-02-28 01:36:34 -0500

    added not found page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <p>
+            ¯\_(ツ)_/¯
+            <br/>
+            Uh oh. There is nothing here.
+        </p>
+    )
+}
+
+export default NoMatch;
