acba185b914b311e20596890ab6172fc7cec7f80
Sihayah <sihaya_a@hotmail.com>
2022-03-02 21:08:38 -0500

    link colors and media queries for mobile-first responsiveness added to css styles

---
+++ src/components/ContactForm/index.js
@@ -0,0 +1,87 @@
+import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const ContactForm = () => {
+    const initialState = {
+        name: "", 
+        email: "", 
+        message: ""    
+    };
+    const [formState, setFormState] = useState(initialState);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [submitted, setSubmitted] = useState("");
+    const { name, email, message } = formState;
+
+    console.log(formState)
+
+    const handleChange = (e) => {
+        const isValid = (e.target.value)
+        if (e.target.name === 'email') {
+            if (!isValid) {
+                setErrorMessage('This email is invalid')
+            } else {
+                setErrorMessage('')
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`)
+                console.log(errorMessage)
+            } else {
+                setErrorMessage('')
+            }
+        }
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value})
+        }
+    }
+    const form = useRef();
+
+    const sendEmail = e => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_cwczzz1', 'template_v7s5abl',form.current, 'I0lKIie4IjqLZ7jBz')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        setSubmitted("Your message has been sent!")
+        setFormState(initialState);
+    };
+
+    return (
+        <form ref={form} onSubmit={sendEmail}>
+        <label htmlFor="name">
+            Name:
+            </label>
+        <input type="text" name="from_name" defaultValue={name} onBlur={handleChange} />
+
+        <label htmlFor="email">
+            Email:
+        </label>
+        <input type="email" name="email" defaultValue={email} onChange={handleChange}/>
+
+        <label htmlFor="message">
+            Message:
+        </label>
+        <textarea name="message" defaultValue={message} onChange={handleChange}/>  
+        {errorMessage && (
+            <div>
+                <p className="error-text danger">
+                    {errorMessage}
+                </p>
+            </div>  
+        )}
+        <button type="submit" value="submit" onChange={handleChange} className="contact-btn">Submit</button>
+        {submitted && (
+            <div>
+                <p>
+                    {submitted}
+                </p>
+            </div>
+        )}
+</form>
+    )
+}
+
+export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,91 +1,15 @@
 import React, { useState, useRef } from 'react';
-import {  } from '../utils/helpers';
-import emailjs from '@emailjs/browser';
+import ContactForm from '../components/ContactForm';
 
 const Contact = () => {
-    const initialState = {
-        name: "", 
-        email: "", 
-        message: ""    
-    };
-    const [formState, setFormState] = useState(initialState);
-    const [errorMessage, setErrorMessage] = useState("");
-    const [submitted, setSubmitted] = useState("");
-    const { name, email, message } = formState;
 
-    const handleChange = (e) => {
-        const isValid = (e.target.value)
-        if (e.target.name === 'email') {
-            if (!isValid) {
-                setErrorMessage('This email is invalid')
-            } else {
-                setErrorMessage('')
-            }
-        } else {
-            if (!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`)
-                console.log(errorMessage)
-            } else {
-                setErrorMessage('')
-            }
-        }
-        if (!errorMessage) {
-            setFormState({...formState, [e.target.name]: e.target.value})
-        }
-    }
-    const form = useRef();
-
-    const sendEmail = e => {
-        e.preventDefault();
-
-            emailjs.sendForm('service_cwczzz1', 'template_v7s5abl',form.current, 'I0lKIie4IjqLZ7jBz')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-            setSubmitted("Your message has been sent!")
-    };
-
-            
     return (
         <section id="contact">
             <div id="form-wrapper">
                 <p className="mb-3">
                     You can contact me here...
                 </p>
-
-                <form ref={form} onSubmit={sendEmail}>
-                        <label htmlFor="name">
-                            Name:
-                            </label>
-                        <input type="text" name="from_name" defaultValue={name} onBlur={handleChange} />
-
-                        <label htmlFor="email">
-                            Email:
-                        </label>
-                        <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
-
-                        <label htmlFor="message">
-                            Message:
-                        </label>
-                        <textarea name="message" defaultValue={message} onBlur={handleChange}/>  
-                        {errorMessage && (
-                            <div>
-                                <p className="error-text danger">
-                                    {errorMessage}
-                                </p>
-                            </div>  
-                        )}
-                        <button type="submit" value="submit" className="contact-btn">Submit</button>
-                        {submitted && (
-                            <div>
-                                <p>
-                                    {submitted}
-                                </p>
-                            </div>
-                        )}
-                </form>
+                <ContactForm />
             </div>
         </section>
 
