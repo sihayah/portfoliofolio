3f072a7955bb618e9f35624c8a6f4b585e53d7f4
sihayah <91216737+sihayah@users.noreply.github.com>
2022-03-29 01:28:26 -0400

    updated link on resume component to download resume

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,14 +4,16 @@ import ContactForm from '../components/ContactForm';
 const Contact = () => {
 
     return (
-        <section id="contact">
-            <div id="form-wrapper">
-                <p className="mb-3">
-                    You can contact me here...
-                </p>
-                <ContactForm />
-            </div>
-        </section>
+        <center>
+            <section id="contact">
+                <div id="form-wrapper">
+                    <p className="mb-3">
+                        You can contact me here...
+                    </p>
+                    <ContactForm />
+                </div>
+            </section>
+        </center>
 
     )
 };
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import pdfIcon from '../assets/pdf-icon.png'
+import { Link } from 'react-router-dom';
+import pdf from '../assets/files/resume.pdf'
 
 const Resume = () => {
     return (
@@ -7,9 +9,9 @@ const Resume = () => {
             <div id="resume">
                 <div id="download-here">
                     <img src={pdfIcon} alt="pdf icon" id="pdf-icon" />                
-                    <a href="../../Resume.pdf">
+                    <Link to ={pdf} target="_blank" download>
                         download my resume here
-                    </a>
+                    </Link>
                 </div>
                 <div className="skills">
 
