b767382c1067b42ba81e334c137c209d7c62b489
sihayah <91216737+sihayah@users.noreply.github.com>
2022-05-03 20:29:25 -0400

    added arrow icons to About

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import portrait from '../../assets/pink_portrait.png';
 import '../About/style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 
 const About = () => {
     return (
         <section className="about">
             <img alt="portrait" className="smiley" src={portrait}/>
-            <div className="intro">   
+            <div className="intro">
+                <span id='hello-there'>
+                    <h3>
+                    Hello there 
+                    </h3> 
+                    <FontAwesomeIcon className='arrow' id='right' icon= { faArrowRight } />
+                    <FontAwesomeIcon className='arrow' id='down' icon= { faArrowDown } />
+                </span>   
+                   
+              
                 <p>
-                Hello, there. I'm Sihaya Harris
+                I'm Sihaya Harris
                 a dynamic full-stack web developer with a background in art & design. I am experienced in MERN stack. I am based out of Brooklyn, and I'm currently seeking out my next endeavor.
                 </p>                           
             </div>
