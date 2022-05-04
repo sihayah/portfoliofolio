59571805d9ca14be2d2add2047a1e3692f4de41b
sihayah <91216737+sihayah@users.noreply.github.com>
2022-05-03 21:39:32 -0400

    color scheme changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,19 +9,22 @@ import '../Header/style.css';
 const Header = () => {
 
     return(
-        <header id="header">
-            <Link to="/">
-                
-                <span className="top d-flex flex-column align-items-baseline">
-                    <h1>
-                    <FontAwesomeIcon icon={ faUserAstronaut } />
-                     Sihaya Harris
-                    </h1>
-                    <h2>web developer</h2>
-                </span>
-            </Link>
-            <Nav />
-        </header>
+        <div id='header-container'>
+            <header id="header">
+                <Link to="/">
+                    
+                    <span className="top d-flex flex-column align-items-baseline">
+                        <h1>
+                        <FontAwesomeIcon id='astronaut' icon={ faUserAstronaut } />
+                        Sihaya Harris
+                        </h1>
+                        <h2>web developer</h2>
+                    </span>
+                </Link>
+                <Nav />
+            </header>            
+        </div>
+
     )    
 }
 
