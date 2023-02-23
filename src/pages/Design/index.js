335b4f976f7a829862b96414eca3dcb686ee25f2
sihayah <91216737+sihayah@users.noreply.github.com>
2023-02-23 03:32:31 -0500

    updated design comp with more case studies

--- src/pages/Design/index.js
+++ src/pages/Design/index.js
@@ -8,12 +8,29 @@ const Design = () => {
     const caseStudies = [
         {
             title: "Planting",
-            mockup: "planting_empathy_map.jpg",
-            slides: "https://docs.google.com/presentation/d/1dOjgu_sHbX1tURTPiMInShJgPFo20LncTjsSOoawQow/edit?usp=sharing",
+            slides: "planting_empathy_map.jpg",
+            gooogleSlides: "https://docs.google.com/presentation/d/1dOjgu_sHbX1tURTPiMInShJgPFo20LncTjsSOoawQow/edit?usp=sharing",
             description: "Guide to gardening, house plant care, and forum for online community of gardeners of all knowledge levels.",
             technologies: "Figma, Google Slides",
             id: "Planting"
         },
+        {
+            title: "QRAD",
+            slides: "qrad_empathy_map.jpg",
+            gooogleSlides: "https://docs.google.com/presentation/d/1dOjgu_sHbX1tURTPiMInShJgPFo20LncTjsSOoawQow/edit?usp=sharing",
+            description: "This app allows users to generate a digital business card with a QR code they can share from the profile, and add other business cards to a contact list.",
+            technologies: "Figma, Google Slides",
+            id: "Planting"
+        },
+        {
+            title: "Budget Tracker",
+            slides: "budget_tracker_empathy_map.jpg",
+            gooogleSlides: "https://docs.google.com/presentation/d/1dOjgu_sHbX1tURTPiMInShJgPFo20LncTjsSOoawQow/edit?usp=sharing",
+            description: "This app allows the user to input and graph income and expenses. User data persists, and offline functionality allows the user to input transactions even without an internet connection.",
+            technologies: "Figma, Google Slides",
+            id: "Planting"
+        },
+
 
     ];
 
@@ -48,12 +65,12 @@ const Design = () => {
                 <div className="portfolio-container">
                     {caseStudies.map ((project) => (
                         <div className="project-container" key={project.title}>
-                            <img className="project-example" id={project.id} src={require(`../../assets/mockups/${project.mockup}`)} alt={project.title}
+                            <img className="project-example" id={project.id} src={require(`../../assets/mockups/${project.slides}`)} alt={project.title}
                             ></img>                                
                             <div className='project-details'>
                                 <h4>{project.title}</h4>
                                 <span id="project-links">
-                                    <a href={project.slides}>Slides</a>
+                                    <a href={project.googleSlides}>Google Slides</a>
                                     <span> / </span>   
                                 </span>  
                                 <p>
