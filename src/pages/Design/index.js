ca0a703be95acdbe698901e202294382517155b8
sihayah <91216737+sihayah@users.noreply.github.com>
2023-03-29 08:53:53 -0400

    eliminated usused js in design page

--- src/pages/Design/index.js
+++ src/pages/Design/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-scroll';
 import ScrollTop from '../../components/ScrollTop';
 import Popup from 'reactjs-popup';
