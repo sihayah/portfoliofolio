89c8d4e82152927349a607a99f1d330ee9928b80
sihayah <91216737+sihayah@users.noreply.github.com>
2023-07-18 21:53:27 -0400

    updated images and css in bbyy page

--- src/pages/Bbyy/index.js
+++ src/pages/Bbyy/index.js
@@ -8,12 +8,12 @@ const Bbyy = () => {
         <section>
             <div className='project-details'>
                 <div className='top-container'>                        
-                    <div>
-                        <img className="project-example logo" id="bbyy" src={require(`../../assets/ux/bbyy_logo_redviolet.webp`)} alt="bbyy">
-                        </img> 
-                        <h4>BBYY</h4>
-                        <h5>(UX/UI)</h5>      
-                    </div>
+                <div className='logo-container'>
+                            <img className="project-example logo" id="bbyy" src={require(`../../assets/ux/bbyy_logo_redviolet.webp`)} alt="planting">
+                            </img>  
+                            <h4>Planting</h4>
+                            <h5>(UX/UI)</h5> 
+                        </div>
                     <article>
                         This is an online shop for handmade, functional and sculptural pottery and various other home goods.
                     </article>
@@ -29,10 +29,112 @@ const Bbyy = () => {
                 <div className='heading-wrapper'>
                        Research
                 </div>
-                <div className='extra-room'>
-                    <img className="project-example personas" src={require(`../../assets/ux/bbyy_personas.webp`)}alt="personas">
-                    </img> 
-                </div> 
+
+                                    {/* Persona 1 */}
+                                    <div className='extra-room persona'>
+                        <div className='persona-intro'>
+                            <img className="persona-graphic" src={require(`../../assets/ux/bbyy_persona1.webp`)}alt="personas">
+                            </img>  
+                            <ul>
+                                <li className='persona-name'>Xena Garcia</li>
+                                <li>(she/her)</li>
+                                <li>Age: 31</li>
+                                <li>Education: BA</li>
+                                <li>Job title: Copywriter</li>
+                                <li>Status: Single</li>
+                                <li>Location: San Jose, CA</li>
+                            </ul>   
+                            <div className='persona-quote'>
+                            “I like to choose objects that feel special to keep in my space.”
+                            </div>
+                        </div>
+                        <div className='persona-lists'>
+                            <div className='list-title-wrapper'><h6>Goals</h6></div>
+                            
+                            <ul>
+                                <li>
+                                    purchase ethically produced goods
+                                </li>
+                                <li>
+                                    find gifts for holidays                                </li>
+                                <li>
+                                    augment the design choices in her home
+                                </li>
+                                <li>
+                                    deal wifind creative storage solutionsth any hiccups like pests, fungal infections, and diseases
+                                </li>
+                            </ul>    <div className='list-title-wrapper'>  <h6>Interests</h6></div>
+                          
+                            <ul>
+                                <li>
+                                    interior design
+                                </li>
+                                <li>
+                                    fine arts
+                                </li>
+                                <li>
+                                    film
+                                </li>
+                                <li>
+                                    photography
+                                </li>
+                                <li>
+                                    history
+                                </li>
+                            </ul>
+                        </div>                    
+                    </div> 
+
+                    {/* Persona 2 */}
+                    <div className='extra-room persona'>
+                        <div className='persona-intro'>
+                            <img className="persona-graphic" src={require(`../../assets/ux/bbyy_persona2.webp`)}alt="personas">
+                            </img>  
+                            <ul>
+                                <li className='persona-name'>Quinn Singh</li>
+                                <li>(he/him)</li>
+                                <li>Age: 55</li>
+                                <li>Education: BFA</li>
+                                <li>Job title: Social Media Manager</li>
+                                <li>Status: Single</li>
+                                <li>Location: Chicago, IL</li>
+                            </ul>   
+                            <div className='persona-quote'>
+                                “I’m always looks for a birthday or house warming gift for someone. It’s difficult to find something meaningful and useful”                            </div>
+                        </div>
+                        <div className='persona-lists'>
+                            <div className='list-title-wrapper'><h6>Goals</h6></div>
+                            
+                            <ul>
+                                <li>
+                                    find one-of-kind artworks to decorate home and office
+                                </li>
+                                <li>
+                                    find gifts                                </li>
+                                <li>
+                                    collect interesting dinner ware for home                                </li>
+                                <li>
+                                    find inspiring pieces                                </li>
+                            </ul>    <div className='list-title-wrapper'>  <h6>Interests</h6></div>
+                          
+                            <ul>
+                                <li>
+                                    fashion
+                                </li>
+                                <li>
+                                    media
+                                </li>
+                                <li>
+                                   humor
+                                </li>
+                                <li>
+                                    entertaining
+                                </li>
+
+                            </ul>
+                        </div>                    
+                    </div> 
+
                 <article className='p'>
                     Foundational data for this project was collected through a handful of interviews of online shoppers. Their feelings regarding similar shopping platforms and the goods available through those platforms were recorded and analyzed. Personas were modeled upon this sample of data.
                 </article>  
@@ -85,14 +187,22 @@ const Bbyy = () => {
                 </div>        
                 <div className='heading-wrapper'>
                        Prototype
-                    </div> 
+                </div> 
+                <div className='extra-room'> 
+                        <img className="design-system" src={require(`../../assets/ux/bbyy_design_system.webp`)}alt="design-system">
+                        </img> 
+                    </div>   
                 <article className='p'>
                     The home page presents a featured image along with a button that can be selected to start shopping. The hamburger menu drops down to offer a search bar and a list of links to the sites sections including different product categories, a contact page, and an about page. When a category is selected, a product list appears with a filtering option at the top. When a product is selected, the product detail page appears. If the shopper chooses to add an item to their cart, a pop-up prompt leads either to the cart or back to the product page. From the cart, the checkout process may be completed by adding shipping and payment info..
                 </article>  
-                <div className='extra-room'>
-                    <img className="project-example" src={require(`../../assets/ux/bbyy_prototype.webp`)}alt="prototype">
-                    </img> 
-                </div>  
+                <div className='extra-room prototypes'>
+                        <img className="project-example" src={require(`../../assets/ux/bbyy_prototype_home.webp`)}alt="prototype">
+                        </img> 
+                        <img className="project-example" src={require(`../../assets/ux/bbyy_prototype_shopnew.webp`)}alt="prototype">
+                        </img> 
+                        <img className="project-example" src={require(`../../assets/ux/bbyy_prototype_productpage.webp`)}alt="prototype">
+                        </img> 
+                    </div> 
                 <a href="https://www.figma.com/proto/Th48j5ohvsGSj1p1h3neBq/prototype?node-id=2-152&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A100" target= "_blank" rel= "noreferrer">Click here to interact with the live prototype.</a>  
                 <article className="summary">
                 
