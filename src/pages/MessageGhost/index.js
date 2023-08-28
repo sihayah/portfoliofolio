f9dc1ed218be43914cdccbaf959cfa71d002acaf
sihayah <91216737+sihayah@users.noreply.github.com>
2023-08-28 19:19:56 -0400

    added updated images to mg page

--- src/pages/MessageGhost/index.js
+++ src/pages/MessageGhost/index.js
@@ -9,13 +9,13 @@ const MessageGhost = () => {
                 <div className='project-details'>
                     <div className='top-container'>
                         <div className='logo-container'>
-                            <img className="project-example logo" id="MessageGhost" src={require(`../../assets/ux/planting_logo.webp`)} alt="MessageGhost">
+                            <img className="project-example logo" id="MessageGhost" src={require(`../../assets/ux/mg_logo.webp`)} alt="MessageGhost">
                             </img>  
                             <h4>MessageGhost</h4>
                             <h5>(UX/UI)</h5> 
                         </div>
                         <article>
-                            MessageGhost is ...
+                            MessageGhost is a responsive website for the purpose of message composition with an AI auto-complettion feature.
                         </article> 
                     </div>
                    
@@ -32,7 +32,7 @@ const MessageGhost = () => {
                     {/* Persona 1 */}
                     <div className='extra-room persona'>
                         <div className='persona-intro'>
-                            <img className="persona-graphic" src={require(`../../assets/ux/persona_graphic_1.webp`)}alt="personas">
+                            <img className="persona-graphic" src={require(`../../assets/ux/mg_persona2.webp`)}alt="personas">
                             </img>  
                             <ul>
                                 <li className='persona-name'>Devon Jung</li>
@@ -85,7 +85,7 @@ const MessageGhost = () => {
                     {/* Persona 2 */}
                     <div className='extra-room persona'>
                         <div className='persona-intro'>
-                            <img className="persona-graphic" src={require(`../../assets/ux/persona_graphic_2.webp`)}alt="personas">
+                            <img className="persona-graphic" src={require(`../../assets/ux/mg_persona1.webp`)}alt="personas">
                             </img>  
                             <ul>
                                 <li className='persona-name'>Malcolm Anderson</li>
@@ -168,14 +168,14 @@ const MessageGhost = () => {
                         Architecture
                     </div>
                     <div>
-                        <img className="project-example sitemap" src={require(`../../assets/ux/sitemap.webp`)}alt="sitemap">
+                        <img className="project-example sitemap" src={require(`../../assets/ux/mg_sitemap.webp`)}alt="sitemap">
                         </img>
                     </div> 
                     <article className='p'>
                         The navigation...
                     </article>                          
                     <div>
-                        <img className="project-example userflow" src={require(`../../assets/ux/userflow.webp`)}alt="userFlow">
+                        <img className="project-example userflow" src={require(`../../assets/ux/mg_userflow.webp`)}alt="userFlow">
                         </img> 
                     </div>   
 
@@ -186,7 +186,11 @@ const MessageGhost = () => {
                         Wireframes laid out desktop, mobile, and tablet 
                     </article>  
                     <div className='extra-room'> 
-                        <img className="wireframe" src={require(`../../assets/ux/wireframe.webp`)}alt="wireframe">
+                        <img className="wireframe" src={require(`../../assets/ux/mg_mobile_wireframes.webp`)}alt="wireframe">
+                        </img> 
+                    </div>   
+                    <div className='extra-room'> 
+                        <img className="wireframe" src={require(`../../assets/ux/mg_desktop_wireframes.webp`)}alt="wireframe">
                         </img> 
                     </div>   
      
@@ -194,18 +198,28 @@ const MessageGhost = () => {
                         Prototype
                     </div> 
                     <div className='extra-room'> 
-                        <img className="design-system" src={require(`../../assets/ux/planting_design_system.webp`)}alt="design-system">
+                        <img className="design-system" src={require(`../../assets/ux/mg_design_system.webp`)}alt="design-system">
                         </img> 
                     </div>   
                     <article className='p'>
                         The prototype was designed...
                     </article>  
                     <div className='extra-room prototypes'>
-                        <img className="project-example" src={require(`../../assets/ux/planting_prototype_home.webp`)}alt="prototype">
+                        {/* mobile prototype */}
+                        <img className="project-example" src={require(`../../assets/ux/mg_prototype_login.webp`)}alt="prototype">
                         </img> 
-                        <img className="project-example" src={require(`../../assets/ux/planting_prototype_mygarden.webp`)}alt="prototype">
+                        <img className="project-example" src={require(`../../assets/ux/mg_prototype_dash.webp`)}alt="prototype">
                         </img> 
-                        <img className="project-example" src={require(`../../assets/ux/planting_prototype_search.webp`)}alt="prototype">
+                        <img className="project-example" src={require(`../../assets/ux/mg_prototype_comp.webp`)}alt="prototype">
+                        </img> 
+                        <img className="project-example" src={require(`../../assets/ux/mg_prototype_metrics.webp`)}alt="prototype">
+                        </img> 
+                        {/* desktop prototype */}
+                        <img className="project-example" src={require(`../../assets/ux/mg_prototype_desktop_login.webp`)}alt="prototype">
+                        </img> 
+                        <img className="project-example" src={require(`../../assets/ux/mg_prototype_desktop_dash.webp`)}alt="prototype">
+                        </img> 
+                        <img className="project-example" src={require(`../../assets/ux/mg_prototype_desktop_comp.webp`)}alt="prototype">
                         </img> 
                     </div>  
                     <a href="https://www.figma.com/proto/kPOzAOly1UEYep7yPG9ne0/prototype?type=design&node-id=626-7028&t=nis7aBnZnprOG0s9-0&scaling=scale-down&page-id=0%3A1&starting-point-node-id=626%3A7130" target= "_blank" rel= "noreferrer">Click here to interact with the live prototype.</a>  
