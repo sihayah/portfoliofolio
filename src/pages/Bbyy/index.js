fc72df81d0f27b0f58aee8027621ac2811dc1671
sihayah <91216737+sihayah@users.noreply.github.com>
2023-09-13 15:03:44 -0400

    updated bbyy content

--- src/pages/Bbyy/index.js
+++ src/pages/Bbyy/index.js
@@ -25,29 +25,37 @@ const Bbyy = () => {
                 <article className='p'>
                     I started this project to create an alternative means of selling pottery and other handmade objects. I performed research, planned the architecture and, and executed the design.
                 </article>    
+                <div className='heading-wrapper'>
+                    Problem Statement
+                </div>
+                <article className='p'>
+                    Our user is an online shopper who seeks to purchase high-quality home goods who needs a resource that will provide an easy shopping experience with accurate product descriptions because they enjoy shopping online and decorating their home.
+                </article>    
 
                 <div className='heading-wrapper'>
                        Research
                 </div>
-
-                                    {/* Persona 1 */}
-                                    <div className='extra-room persona'>
-                        <div className='persona-intro'>
-                            <img className="persona-graphic" src={require(`../../assets/ux/bbyy_persona1.webp`)}alt="personas">
-                            </img>  
-                            <ul>
-                                <li className='persona-name'>Xena Garcia</li>
-                                <li>(she/her)</li>
-                                <li>Age: 31</li>
-                                <li>Education: BA</li>
-                                <li>Job title: Copywriter</li>
-                                <li>Status: Single</li>
-                                <li>Location: San Jose, CA</li>
-                            </ul>   
-                            <div className='persona-quote'>
-                            “I like to choose objects that feel special to keep in my space.”
-                            </div>
+                <article className='p'>
+                    Foundational data for this project was collected through a survey of online shoppers. Personas were modeled upon this sample of data.
+                </article> 
+                {/* Persona 1 */}
+                <div className='extra-room persona'>
+                <div className='persona-intro'>
+                    <img className="persona-graphic" src={require(`../../assets/ux/bbyy_persona1.webp`)}alt="personas">
+                     </img>  
+                        <ul>
+                            <li className='persona-name'>Xena Garcia</li>
+                            <li>(she/her)</li>
+                            <li>Age: 31</li>
+                            <li>Education: BA</li>
+                            <li>Job title: Copywriter</li>
+                            <li>Status: Single</li>
+                            <li>Location: San Jose, CA</li>
+                        </ul>   
+                        <div className='persona-quote'>
+                        “I like to choose objects that feel special to keep in my space.”
                         </div>
+                    </div>
                         <div className='persona-lists'>
                             <div className='list-title-wrapper'><h6>Goals</h6></div>
                             
@@ -99,11 +107,7 @@ const Bbyy = () => {
                                     find inspiring pieces                                </li>
                             </ul> 
                         </div>                    
-                    </div> 
-
-                <article className='p'>
-                    Foundational data for this project was collected through a handful of interviews of online shoppers. Their feelings regarding similar shopping platforms and the goods available through those platforms were recorded and analyzed. Personas were modeled upon this sample of data.
-                </article>  
+                    </div>  
                 <div className="pain-point-wrapper">
                     <ul className="pain-point-ul">
                         Pain points:
@@ -120,21 +124,21 @@ const Bbyy = () => {
                 </div>
 
                 <article className='p'>
-                    The competitive audit to a look at the design and usability of a sample of shopping sites of both independent artists and small home goods brands. The key competitors audited were Florian Gadsby and Jungalow.The Florian Gadsby shop is super minimal and portfolio-based. This is a cookie cutter artist's portfolio/shopping site that echoes many others. Its very straight-forward. The Jungalow site is more brand and product focused. The aesthetic is playful. It’s beautifully executed with color palettes and graphics that add a stronger sense of the artist. Florian Gadsby offered and undeniably clean and simple layout. The features offered by Jungalow were deeper and more interactive including many sub-menus and filtering options. A balance between these two approaches seems more fitting for this project. Qualitative preference lies with Jungalow site, as it centers the user and the product rather than the creator. But the features and pages for this project will be significantly paired down.
+                    I conducted a competitive audit, reviewing the design and usability of a sample of shopping sites of both independent artists and small home goods brands. The samples audited were Florian Gadsby and Jungalow. The Florian Gadsby shop is super minimal, and as much a portfolio as a shop. It is a cookie cutter artist's portfolio/shopping site that echoes many others. The Jungalow site is more brand and product-focused. The aesthetic is playful and beautifully executed, with color palettes and graphics that add a stronger sense of the brand. Where Florian Gadsby offered a basic layout, the features in Jungalow were deeper and more interactive, including many sub-menus and filtering options. A balance between these two approaches seems more fitting for Blahblahyaya.
                 </article> 
 
                 <div class="audit-link">
                     <a href="https://docs.google.com/spreadsheets/d/1PuH0lh_l1oQKtAUnrkkNNWplvO2stb1MsLcN8MdUY8Y/edit?usp=sharing&resourcekey=0-ITXj9wHlUTiJ-dQTTRZuuw" target= "_blank" rel= "noreferrer">Click here to view the competitve audit in Google Sheets.</a> 
                 </div>
                 <div className='heading-wrapper'>
-                       Architecture
-                    </div>
+                    Architecture
+                </div>
                 <div>
                     <img className="project-example sitemap" src={require(`../../assets/ux/bbyy_sitemap.webp`)}alt="sitemap">
                     </img>
                 </div> 
                 <article className='p'>
-                    The structure of the site evolved over time, starting with the minimal necessary features, and later expanding on those features. This occurred primarily in the checkout portion of the site, further pages with confirmations were necessary to encourage the confidence of shoppers along their journey.
+                    The site's structure evolved around the main user flow, which takes the user from a product page through a checkout process. Mindwandering elements such as carousels and item recommendations add complexity to various pages to encourage the user to view as many products as possible.
                 </article>                          
                 <div>
                     <img className="project-example userflow" src={require(`../../assets/ux/bbyy_userflow.webp`)}alt="userFlow">
@@ -142,10 +146,10 @@ const Bbyy = () => {
                 </div>     
                 
                 <div className='heading-wrapper'>
-                       Wireframing
+                       Wireframes
                     </div>             
                 <article className='p'>
-                    After the blueprint was laid, a multitude of compositional directions were explored through wireframes. The selected wireframe laid out the essential structure of all the core features. The home page was most iterated with a more dynamic experience being established to engage users more quickly.
+                    After the blueprint was laid, I explored many compositional directions through wireframes. The selected wireframe laid out the essential structure of all the core UI. After running the initial usability tests, I iterated the wireframes to include more points of entry for product pages.
                 </article>  
                 <div className='extra-room'> 
                     <img className="project-example" src={require(`../../assets/ux/bbyy_wireframe.webp`)}alt="wireframe">
@@ -159,7 +163,9 @@ const Bbyy = () => {
                         </img> 
                     </div>   
                 <article className='p'>
-                    The home page presents a featured image along with a button that can be selected to start shopping. The hamburger menu drops down to offer a search bar and a list of links to the sites sections including different product categories, a contact page, and an about page. When a category is selected, a product list appears with a filtering option at the top. When a product is selected, the product detail page appears. If the shopper chooses to add an item to their cart, a pop-up prompt leads either to the cart or back to the product page. From the cart, the checkout process may be completed by adding shipping and payment info..
+                    A dark palette background with bright secondary triad colors was employed to echo the design of the products and make the light-colored porcelain pop off the page.
+
+                    The UI is relatively simple, but iterations were necessary to make them feel dynamic. In ongoing development, I will explore different notification elements to guide the user towards shop promotions.
                 </article>  
                 <div className='extra-room prototypes'>
                         <img className="project-example" src={require(`../../assets/ux/bbyy_prototype_home.webp`)}alt="prototype">
